8b9ce257710780432feb7814b0e74975f572e3ea
bruno bastos duarte <bastosduartebruno@gmail.com>
2022-05-27 17:20:00 -0300

    deleting an administrator

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,6 +1,6 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
   before_action :verify_password, only: [:update]
-  before_action :set_admin, only: [:edit, :update]
+  before_action :set_admin, only: [:edit, :update, :destroy]
 
   def index
     @admins = Admin.all
@@ -30,6 +30,15 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
     end
 
   end
+
+  def destroy
+    if @admin.destroy
+    redirect_to admins_backoffice_admins_path, notice: "Administrador excluído com sucesso!"
+    else
+      render :index
+    end
+  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   namespace :admins_backoffice do
     get 'welcome/index' # Dashboard
-    resources :admins, except: [:delete]
+    resources :admins
   end
 
   devise_for :admins
