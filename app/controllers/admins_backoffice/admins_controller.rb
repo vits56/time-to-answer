892c7705c8d856ce06a57ef4b76da3e06447f609
bruno bastos duarte <bastosduartebruno@gmail.com>
2022-05-27 12:34:11 -0300

    finalizing the admin edit form

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -6,4 +6,14 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   def edit
     @admin = Admin.find(params[:id])
   end
+
+  def update
+    admin = Admin.find(params[:id])
+    param_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
+    if admin.update(param_admin)
+      redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
+    else
+      render :edit
+    end
+  end
 end
