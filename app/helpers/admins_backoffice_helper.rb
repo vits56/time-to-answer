5c274645c2e48b5e6cff83f06839b9ac8ac3ccbb
bruno bastos duarte <bastosduartebruno@gmail.com>
2022-05-29 14:27:18 -0300

    incrementing i18n for models

--- app/helpers/admins_backoffice_helper.rb
+++ app/helpers/admins_backoffice_helper.rb
@@ -1,2 +1,10 @@
 module AdminsBackofficeHelper
+  def translate_attribute(object = nil, method = nil)
+    if object && method
+      object.model.human_attribute_name(method)
+    else
+      'Informe os Parâmetros corretamente.'
+    end
+  end
+    
 end
