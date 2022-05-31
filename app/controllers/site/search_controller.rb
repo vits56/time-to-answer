8a7cbdfc95b08271a5fb137225d1cd6596cbe49f
bruno bastos duarte <bastosduartebruno@gmail.com>
2022-05-31 18:03:33 -0300

    Using class methods in the model.

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,9 +1,5 @@
 class Site::SearchController < SiteController
   def questions
-    # params[:term]
-    # @questions = Question.includes(:answers).page(params[:page])
-    @questions = Question.includes(:answers)
-                         .where("lower(description) LIKE ?", "%#{params[:term].downcase}%")
-                          .page(params[:page])
+    @questions = Question.search(params[:page], params[:term])
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -6,4 +6,14 @@ class Question < ApplicationRecord
 
   # Kaminari
   paginates_per 5
+
+  def self.search(page, term)
+    Question.includes(:answers)
+            .where("lower(description) LIKE ?", "%#{term.downcase}%")
+            .page(page)
+  end
+
+  def self.last_questions
+    Question.includes(:answers).order('created_at desc').page(page)
+  end
 end
