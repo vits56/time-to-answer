58b983283ea1aff1805d56ef4ae66c28bb195103
bruno bastos duarte <bastosduartebruno@gmail.com>
2022-06-01 16:44:29 -0300

    Filtering questions by subject.

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,12 @@
 module SiteHelper
+  def msg_jumbotron
+    case params[:action]
+    when 'index'
+      "Últimas perguntas cadastradas..."
+    when 'questions'
+      "Resultados da busca por \"#{params[:term]}\""
+    when 'subject'
+      "Perguntas relacionadas ao assunto \"#{params[:subject]}\"..."
+    end
+  end
 end
