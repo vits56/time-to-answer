b2e437e248b018060a54f7f065f6140fac8b130a
bruno bastos duarte <bastosduartebruno@gmail.com>
2022-06-02 12:23:53 -0300

    Adding extra data.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ApplicationRecord
          :recoverable,
          :rememberable,
          :validatable
+
+  has_one :user_profile
+  accepts_nested_attributes_for :user_profile, reject_if: :all_blank
+
+
   # Validations
   validates :first_name, presence: true, length: { minimum: 3 }, on: :update
 
