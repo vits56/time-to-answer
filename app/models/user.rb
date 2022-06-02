ce525b3625013db24deacd780032feb861f841ca
bruno bastos duarte <bastosduartebruno@gmail.com>
2022-06-02 01:08:58 -0300

    Knowing and using validations in Rails.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,10 @@ class User < ApplicationRecord
          :recoverable,
          :rememberable,
          :validatable
+  # Validations
+  validates :first_name, presence: true, length: { minimum: 3 }, on: :update
 
+  # Virtual attributes
   def full_name
     [self.first_name, self.last_name].join(' ')
   end
