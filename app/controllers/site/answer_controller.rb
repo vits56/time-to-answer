c46b09af5d9daca73c21886f9edcc9c54f8d5b4e
bruno bastos duarte <bastosduartebruno@gmail.com>
2022-06-02 16:55:45 -0300

    Refactoring UserStatistic.

--- app/controllers/site/answer_controller.rb
+++ app/controllers/site/answer_controller.rb
@@ -1,7 +1,7 @@
 class Site::AnswerController < SiteController
-  
   def question
-    @answer = Answer.find(params[:answer_id ])
+    @answer = Answer.find(params[:answer_id])
+    UserStatistic.set_statistic(@answer, current_user)
   end
-
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_02_173754) do
+ActiveRecord::Schema.define(version: 2022_06_02_185343) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -77,6 +77,15 @@ ActiveRecord::Schema.define(version: 2022_06_02_173754) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_user_profiles_on_user_id"
+  end
+
+  create_table "user_statistics", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "right_questions", default: 0
+    t.integer "wrong_questions", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_statistics_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
