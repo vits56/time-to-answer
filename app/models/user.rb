abe950ff826bc00a5f29530ac51daa26ad4d5282
bruno bastos duarte <bastosduartebruno@gmail.com>
2022-06-02 21:17:19 -0300

    Admin Statistics.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,26 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable,
-         :registerable,
-         :recoverable,
-         :rememberable,
-         :validatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   has_one :user_profile
   accepts_nested_attributes_for :user_profile, reject_if: :all_blank
-
+  
+  # Callback
+  after_create :set_statistic
 
   # Validations
   validates :first_name, presence: true, length: { minimum: 3 }, on: :update
-
-  # Virtual attributes
+  
+  # Virtual Attributes
   def full_name
     [self.first_name, self.last_name].join(' ')
+  end
+
+  private
+
+  def set_statistic
+    AdminStatistic.set_event(AdminStatistic::EVENTS[:total_users])
   end
 end
